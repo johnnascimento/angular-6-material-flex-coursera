0fa0332d465103a0ee45113987e5ea3135f1e897
John Lenon Nascimento da Silva <jnascimento@itelios.com>
2020-07-02 09:37:46 -0300

    Starting up with angular@6, Angular flex layout and angular material

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+﻿export class Dish {
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+}
