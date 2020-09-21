2a40013a82322d1fadb48c0384e3cf39f3d87d61
John Nascimento <jnascimento@itelios.com>
2020-09-21 11:46:59 -0300

    Angular 6 - SPA part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -23,6 +23,10 @@ export const routes: Routes = [
         path: 'contactus',
         component: ContactComponent
     },
+    {
+        path: 'dishdetail/:id',
+        component: DishdetailComponent
+    },
     { 
         path: '',
         redirectTo: '/home',
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -13,7 +13,13 @@ export class DishService {
   }
 
   getDish(id: string): Dish {
-    return DISHES.filter((dish) => { (dish.id === id) })[0];
+    let selectedDish = DISHES.filter((dish) => {
+      if(dish.id === id) {
+        return dish;
+      }
+    })[0];
+
+    return selectedDish;
   }
 
   getFeaturedDish() {
